108a07468eabaa2b1600afae268a16fe7fc1a8bc
jstpp <97021244+jstpp@users.noreply.github.com>
2026-05-06 13:28:10 +0200

    Frontend setup, profile page, enclosures

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # eaRSSy - fast, modern RSS reader
 <p align="center">
-    <img src="https://img.shields.io/badge/License-MIT-green.svg?style=for-the-badge" alt="CodeFactor" />
+    <img src="https://img.shields.io/badge/License-MIT-green.svg?style=for-the-badge"/>
     <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/jstpp/earssy?style=for-the-badge">
     <a href="https://www.codefactor.io/repository/github/jstpp/earssy"><img src="https://www.codefactor.io/repository/github/jstpp/earssy/badge?style=for-the-badge" alt="CodeFactor" /></a>
 </p>
--- app/app.py
+++ app/app.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template, request, session
+import mysql.connector
 import feedparser
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def site_main():
-    rss_urls = ["https://feeds.bbci.co.uk/news/rss.xml",
-                "https://www.polsatnews.pl/rss/swiat.xml"] # Temporary URLs for initial setup. Will be replaced by mysql database.
+app.config['MYSQL_HOST'] = 'mysql'
+app.config['MYSQL_USER'] = 'earssy_db'
+app.config['MYSQL_PASSWORD'] = 'earssy_db'
+app.config['MYSQL_DB'] = 'earssy_db'
 
+#db = mysql.connector.connect(user=app.config['MYSQL_USER'], password=app.config['MYSQL_PASSWORD'], database=app.config['MYSQL_DB'])
+#cursor = db.cursor(dictionary=True, buffered=True)
+
+rss_urls = ["https://feeds.bbci.co.uk/news/rss.xml",
+            "https://www.polsatnews.pl/rss/swiat.xml"] # Temporary URLs for initial setup. Will be replaced by mysql database.
+
+def get_context(rss_urls):
     context = {'channels': []}
-
     for url in rss_urls:
         context['channels'].append(feedparser.parse(url))
+    return context
 
+context = get_context(rss_urls)
+
+@app.route("/", methods=['GET'])
+def site_main():
     return render_template('index.html', **context)
+
+
+@app.route("/profile", methods=['GET'])
+def profile():
+    return render_template('profile.html', **context)
